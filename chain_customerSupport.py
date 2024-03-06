c7ee97d9ec279ab56b015b3bd34c7e1be3908012
Zahra Zen Marbun <155308859+zahrazenmarbun@users.noreply.github.com>
2024-03-06 23:45:18 +0700

    Update chain_customerSupport.py

--- chain_customerSupport.py
+++ chain_customerSupport.py
@@ -5,7 +5,7 @@ from langchain.prompts import PromptTemplate
 import os
 
 # Mengatur LLM
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "API KEY KAMU"
 llm = ChatOpenAI(temperature=0.9)
 
 def handle_complaint(komplain: str) -> str:
